458a78733eb7b4cd33dc18de507ce66576923d07
edzaryan <yeghiazaryanedgar93@gmail.com>
2022-12-25 03:46:26 +0400

    Some Changes 5

--- EmployeeApp/Models/EmployeeListModel.cs
+++ EmployeeApp/Models/EmployeeListModel.cs
@@ -6,19 +6,24 @@ namespace EmployeeApp.Models
     {
         public int Id { get; set; }
 
+
         [Required(ErrorMessage = "Please enter the {0}")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "{0} length must be between {2} to {1}")]
         public string Name { get; set; }
+
 
         [Required(ErrorMessage = "Please enter the {0}")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "{0} length must be between {2} to {1}")]
         public string Surname { get; set; }
 
+
         [Required(ErrorMessage = "Please enter the {0}")]
         [EmailAddress]
         public string Email { get; set; }
 
+
         public string Department { get; set; }
+
 
         public string? ImageFileName { get; set; }
     }
